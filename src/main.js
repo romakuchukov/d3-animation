aab2d6fa9e64697a3fc247635a3b2919de39b034
Roma K <romankuchukov@gmail.com>
2020-09-16 19:52:15 -0400

    randomness in animation

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
-const svg = d3.select('body').append('svg').attr('width', 960).attr('height', 500);
-const timeCircle = svg.append('circle').attr('r', 20).attr('cy', 250).attr('cx', 40).attr('fill', 'steelblue');
+const data = [];
 
+let i = 100;
+while (i--) data.push(i);
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+
+const svg = d3.select('body').append('svg').attr('height', '100%').attr('width', '100%')
+
+const rects = svg.selectAll('.rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('width', 2)
+  .attr('height', 2)
+  .attr('x', d => d)
+  .attr('y', 250)
+  .attr('fill', 'steelblue');
+
+//svg.append('rect')
+
+console.log();
+//rand(1, 10)*1000
 const repeat = () => {
-  timeCircle
+  rects
     .transition()
-    .duration(5000)
+    .duration(() => rand(1, 10)*1000)
     .attr('fill', 'red')
     .transition()
-    .duration(5000)
+    .duration(500)
     .attr('fill', 'steelblue')
     .on('end', repeat);
 };
