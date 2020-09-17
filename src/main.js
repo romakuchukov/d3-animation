478aa7e195ac035ee1e4c25d8316301800a1be97
Roma K <romankuchukov@gmail.com>
2020-09-16 23:32:48 -0400

    animating fill color

--- src/main.js
+++ src/main.js
@@ -1,28 +1,38 @@
 const data = [];
 
-let i = 100;
+let i = 10;
 while (i--) data.push(i);
 
 const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 
-const svg = d3.select('body').append('svg').attr('height', '100%').attr('width', '100%')
+const svg = d3.select('body').append('svg').attr('height', '100%').attr('width', '100%');
 
 const rects = svg.selectAll('.rect')
   .data(data)
   .enter()
   .append('rect')
-  .attr('width', 2)
-  .attr('height', 2)
-  .attr('x', d => d)
+  .attr('width', 5)
+  .attr('height', 5)
+  .attr('x', d => d*5)
   .attr('y', 250)
   .attr('fill', 'steelblue');
 
-//svg.append('rect')
 
-console.log();
-//rand(1, 10)*1000
+// for (const i of data) {
+//   rects.data(data)
+//     .enter()
+//     .append('rect')
+//     .attr('width', 5)
+//     .attr('height', 5)
+//     .attr('x', i*5)
+//     .attr('y', d => d*5)
+//     .attr('fill', 'steelblue');
+// }
+
+
 const repeat = () => {
+
   rects
     .transition()
     .duration(() => rand(1, 10)*1000)
