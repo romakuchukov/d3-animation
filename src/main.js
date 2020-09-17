6611e2af00b5f69a1e9da4219d49955108eaaecd
Roma K <romankuchukov@gmail.com>
2020-09-16 23:45:00 -0400

    looping

--- src/main.js
+++ src/main.js
@@ -9,38 +9,38 @@ const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const svg = d3.select('body').append('svg').attr('height', '100%').attr('width', '100%');
 
 const rects = svg.selectAll('.rect')
-  .data(data)
-  .enter()
-  .append('rect')
-  .attr('width', 5)
-  .attr('height', 5)
-  .attr('x', d => d*5)
-  .attr('y', 250)
-  .attr('fill', 'steelblue');
+  // .data(data)
+  // .enter()
+  // .append('rect')
+  // .attr('width', 5)
+  // .attr('height', 5)
+  // .attr('x', d => d*5)
+  // .attr('y', 250)
+  // .attr('fill', 'steelblue');
 
 
-// for (const i of data) {
-//   rects.data(data)
-//     .enter()
-//     .append('rect')
-//     .attr('width', 5)
-//     .attr('height', 5)
-//     .attr('x', i*5)
-//     .attr('y', d => d*5)
-//     .attr('fill', 'steelblue');
-// }
-
-
-const repeat = () => {
-
-  rects
+for (const i of data) {
+  rects.data(data)
+    .enter()
+    .append('rect')
+    .attr('width', 5)
+    .attr('height', 5)
+    .attr('x', i*5)
+    .attr('y', d => d*5)
+    .attr('fill', 'steelblue')
     .transition()
     .duration(() => rand(1, 10)*1000)
     .attr('fill', 'red')
     .transition()
-    .duration(500)
+    .duration(5000)
     .attr('fill', 'steelblue')
     .on('end', repeat);
+}
+
+
+const repeat = () => {
+
+  rects.on('end', repeat);
 };
 
 repeat();
